2aff37f7e45c760b7af1a13ae3a0d29398040a2e
gyurgyma <gyurgyma@mail.uc.edu>
2018-03-03 15:49:54 -0500

    Started the implementation

--- PhotoArcade.py
+++ PhotoArcade.py
@@ -2,13 +2,13 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 
-class PhotoArcadeMainMenu(Widget):
+class PhotoArcadeMenu(Widget):
     pass
 
 
 class PhotoArcadeApp(App):
     def build(self):
-        return PhotoArcadeMainMenu()
+        return PhotoArcadeMenu()
 
 
 if __name__ == '__main__':
