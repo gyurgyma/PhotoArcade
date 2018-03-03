fe256f97748b7ec7377313f2899f9dbfc22dbff3
gyurgyma <gyurgyma@mail.uc.edu>
2018-03-03 17:38:17 -0500

    Michael Wins

--- PhotoArcade.py
+++ PhotoArcade.py
@@ -1,9 +1,20 @@
+from kivy.properties import NumericProperty
+
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.clock import Clock
+from kivy.graphics import *
 
 class PhotoArcadeMenu(Widget):
-    pass
+    count = NumericProperty(0)
+
+
+    def __init__(self):
+        Widget.__init__(self)
+        Clock.schedule_interval(self.main_loop, 0.05)
+
+    def main_loop(self, deltaTime):
+        self.count = self.count + 1
 
 
 class PhotoArcadeApp(App):
