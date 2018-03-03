430682e31102cf8f3cf38ba906a3f1f2bf264437
gyurgyma <gyurgyma@mail.uc.edu>
2018-03-03 17:47:42 -0500

    converted the strings to number.

--- PhotoArcade.py
+++ PhotoArcade.py
@@ -7,19 +7,25 @@ from kivy.clock import Clock
 
 
 class PhotoArcadeMenu(BoxLayout):
+
     def __init__(self):
         BoxLayout.__init__(self)
+
         button = Button()
-        button.text = "abcd"
+        self.number = 0
+
+        button.text = str(self.number)
         button.size_hint_max_x = 1000
         button.size_hint_max_y = 1000
+
         self.add_widget(button)
         self.button_stuff = Button()
         self.add_widget(self.button_stuff)
         Clock.schedule_interval(self.main_game_loop, 0.5)
 
     def main_game_loop(self, dt):
-        self.button_stuff.text = self.button_stuff.text + "1"
+        self.number = self.number + 1
+        self.button_stuff.text = str(self.number)
 
 
 class PhotoArcadeApp(App):
