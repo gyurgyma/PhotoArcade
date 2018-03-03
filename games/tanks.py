21dff10f39366ea0eb9333f9459eabe8f1e94cf2
flounderK <clifwolfe1@gmail.com>
2018-03-03 14:31:16 -0500

    added vector

--- games/tanks.py
+++ games/tanks.py
@@ -6,14 +6,19 @@ class game_object(ABC):
         self.x = x
         self.y = y
     
-
+#
 class shell(game_object):
-    def __init__(self):
+    def __init__(self, vector):
         super(shell, self).__init__()
-        
+        self.vector = vector
 
 class tank(game_object):
     def __init__(self):
         super(tank, self).__init__()
 
-    
+    def _move(self):
+        pass
+
+    def _shoot(self):
+        pass
+        #create shell object
